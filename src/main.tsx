927b1079372eff766d312fe6ff97a2de38ced9d6
Vinura Dulaksith Kandage <dulaksith@gmail.com>
2025-08-31 07:27:00 +0530

    Implement dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from "@/components/ui/provider"
-import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeProvider } from "@/components/ui/color-mode"
+import { ChakraProvider, Theme ,defaultSystem } from "@chakra-ui/react";
 import App from './App.tsx'
+
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
+    </ChakraProvider>
   </StrictMode>,
 )
