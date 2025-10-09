4a6111aaa43cd39875d5c632faecfb013b2c38ac
Vinura Dulaksith Kandage <dulaksith@gmail.com>
2025-10-09 20:25:22 +0530

    Display critic score

--- src/components/ui/GameCard.tsx
+++ src/components/ui/GameCard.tsx
@@ -1,8 +1,9 @@
 import type { Game } from "@/hooks/useGames";
-import { Card, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { useColorMode } from "./color-mode";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -18,9 +19,12 @@ const GameCard = ({ game }: Props) => {
         backgroundColor={colorMode === "light" ? "black.100" : "black.900"}
       >
         <Heading size="2xl">{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between" marginTop={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </Card.Body>
     </Card.Root>
   );
